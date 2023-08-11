54878aab6a5487da6bd4d984d1a476aacbde29e1
Bibaswan Prasai <bibaswanprasai12@gmail.com>
2023-08-11 12:24:19 +0545

    services section added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
 import About from "../Layout/About";
+import Services from "../Layout/Services";
 
 export default function Home(){
     return(
+        <>
+        
         <About />
+        
+        <Services />
+        </>
     )
 }
